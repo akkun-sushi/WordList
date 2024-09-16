a6def05ee441bf5c7311054182af200589d42e83
akkun-sushi <akkun10@outlook.jp>
2024-09-16 22:48:45 +0900

    c

--- src/app/api/record/route.ts
+++ src/app/api/record/route.ts
@@ -2,7 +2,6 @@ import { supabase } from "@/utils/supabaseClient";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-/*
 export async function GET(req: Request, res: NextApiResponse) {
   const { data, error } = await supabase.from("record").select("*");
 
@@ -28,40 +27,4 @@ export async function POST(req: Request, res: NextApiResponse) {
   }
 
   return NextResponse.json(data);
-}
-*/
-
-
-// CORS ヘッダーを設定する関数
-const setCorsHeaders = (res: NextApiResponse) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // すべてのオリジンを許可
-  res.setHeader("Access-Control-Allow-Headers", "authorization, x-client-info, apikey, content-type");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-};
-
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // CORS のオプションリクエストに対応
-  if (req.method === "OPTIONS") {
-    setCorsHeaders(res);
-    res.status(200).end();
-    return;
-  }
-
-  setCorsHeaders(res);
-
-  if (req.method === "GET") {
-    try {
-      const { data, error } = await supabase.from("record").select("*");
-
-      if (error) {
-        return res.status(500).json({ error: error.message });
-      }
-
-      return res.status(200).json(data);
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-  } else {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
 }
